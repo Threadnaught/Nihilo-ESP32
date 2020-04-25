ef8d5e45e89588370d6d088420113d35fc6068be
idk <patrickhingley@live.co.uk>
2020-04-25 16:44:31 +0100

    updated nihserv for dup correction

--- nihtool/server.py
+++ nihtool/server.py
@@ -1,15 +1,21 @@
 from http import server
 
-data = "\n"
+data = []
+
 
 class NihAddressHanlder(server.BaseHTTPRequestHandler):
 	def do_GET(self):
 		global data
-		self.wfile.write(data.encode())
+		tosend = data
+		output_raw = str(len(tosend)) + '\n' + '\n'.join([x[0] + ':' + x[1] for x in tosend])
+		self.wfile.write(output_raw.encode())
 		self.send_response(200)
 	def do_POST(self):
 		global data
-		data = data + self.rfile.read(int(self.headers['Content-Length'])).decode() + "\n"
+		input_raw = self.rfile.read(int(self.headers['Content-Length'])).decode()
+		IP, ID = input_raw.split(':')
+		data = [x for x in data if x[0] != IP and x[1] != ID] #remove duplicates
+		data.append((IP,ID))
 		self.send_response(200)
 		self.end_headers()
 		
